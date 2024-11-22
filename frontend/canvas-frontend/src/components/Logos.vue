77d8b14f55feb283aca12d04803d7df70dfe35f8
IMYXR <a609634856@outlook.com>
2024-11-22 11:47:28 -0500

    First Upload the frontend for this project

---
+++ frontend/canvas-frontend/src/components/Logos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <a href="https://github.com/DFY-NCSU/CoinCanvas" target="_blank">
+      <img src="../assets/logo-canvas.png" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.logo.element-plus:hover {
+  filter: drop-shadow(0 0 2em #409effaa);
+}
+</style>
